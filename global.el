21d4400d540c5963692242fab9abaeb586416770
starenka <starenka0@gmail.com>
2022-10-25 00:00:55 +0200

    use zone

--- global.el
+++ global.el
@@ -241,6 +241,5 @@
   :ensure t
   :config
   (setq zone-programs [zone-pgm-jitter zone-pgm-putz-with-case zone-pgm-whack-chars zone-pgm-rotate zone-pgm-rotate-LR-lockstep zone-pgm-rotate-RL-lockstep zone-pgm-rotate-LR-variable zone-pgm-rotate-RL-variable zone-pgm-drip zone-pgm-drip-fretfully zone-pgm-five-oclock-swan-dive zone-pgm-rat-race zone-pgm-paragraph-spaz zone-pgm-random-life])
-  ;;(zone-when-idle 90)
+  (zone-when-idle 120)
 )
-
