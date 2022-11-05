a7376a0acf1db860e3a5d347cb556b11b854d806
starenka <starenka0@gmail.com>
2022-11-05 18:45:06 +0100

    cleanup

--- visage.el
+++ visage.el
@@ -1,3 +1,12 @@
+(set-face-attribute 'default nil
+                    :family "DejaVu Sans Mono"
+                    :height 80
+                    :weight 'normal
+                    :width 'normal)
+
+(copy-face 'default 'fixed-pitch)
+
+
 (use-package solarized-theme :ensure t)
 (use-package seoul256-theme :ensure t)
 (use-package nimbus-theme :ensure t)
@@ -8,48 +17,34 @@
   :config
   (custom-theme-set-faces
    'twilight-bright
-   '(deadgrep-match-face ((nil :foreground "black" :background "#e3f4ff")))))
+   '(deadgrep-match-face ((nil :foreground "black" :background "#e3f4ff"))))
+  (load-theme 'twilight-bright t))
+
+(custom-theme-set-faces
+   'twilight-bright
+   '(deadgrep-match-face ((nil :foreground "black" :background "#e3f4ff"))))
+;;(set-face-attribute 'deadgrep-match-face nil :foreground "black" :background "#e3f4ff") ;; https://www.reddit.com/r/emacs/comments/4v7tcj/comment/d5wyu1r/?utm_source=reddit&utm_medium=web2x&context=3
 
 (use-package nano-theme
   :ensure t
+  :defer
   :config
   (custom-theme-set-faces
    'nano-light
    '(mode-line-buffer-id ((nil :foreground "black")))
    '(deadgrep-match-face ((nil :foreground "black" :background "#b4eeb4")))
-   '(selectrum-current-candidate ((nil foreground "black" :background "#b4eeb4")))))
+   '(selectrum-current-candidate ((nil foreground "black" :background "#b4eeb4"))))
+  (load-theme 'nano-light t))
 
 (use-package monokai-theme
   :ensure t
   :pin melpa
+  :defer
   :config
   (custom-theme-set-faces
    'monokai
    '(mode-line-buffer-id ((nil :foreground "goldenrod1")))
    '(deadgrep-match-face ((nil :foreground "black" :background "goldenrod1")))
-   '(selectrum-current-candidate ((nil :foreground "black" :background "goldenrod1")))))
-
-(load-theme 'twilight-bright t)
-;;(load-theme 'monokai t)
-
-
-(set-face-attribute 'default nil
-                    :family "DejaVu Sans Mono"
-                    :height 80
-                    :weight 'normal
-                    :width 'normal)
-
-(copy-face 'default 'fixed-pitch)
-
-
-;;(set-face-attribute 'helm-source-header nil
-;;                    :background "#272822"
-;;                    :foreground "goldenrod1"
-;;                    :underline nil)
-
-;;(set-face-background 'idle-highlight "#3E3D31")
-;;(set-face-background 'idle-highlight "goldenrod")
-;;(set-face-background 'idle-highlight "#c9acff")
-
-
+   '(selectrum-current-candidate ((nil :foreground "black" :background "goldenrod1"))))
+  (load-theme 'monikai t))
 
