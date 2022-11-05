2443a6452e1ee5887a519b2e007ff65853b68da7
starenka <starenka0@gmail.com>
2022-11-05 20:20:25 +0100

    cleanup visage

--- visage.el
+++ visage.el
@@ -6,14 +6,15 @@
 
 (copy-face 'default 'fixed-pitch)
 
-
-(use-package solarized-theme :ensure t)
-(use-package seoul256-theme :ensure t)
-(use-package nimbus-theme :ensure t)
-(use-package leuven-theme :ensure t)
+;;(use-package solarized-theme :ensure t)
+;;(use-package seoul256-theme :ensure t)
+;;(use-package nimbus-theme :ensure t)
+;;(use-package leuven-theme :ensure t)
+;;((use-package nano-theme :ensure t)
 
 (use-package twilight-bright-theme
   :ensure t
+  ;;:defer
   :config
   (load-theme 'twilight-bright t)
   ;; (set-face-attribute 'deadgrep-match-face nil :foreground "#cf7900" :background "#fdf9f2")
@@ -25,17 +26,6 @@
    '(deadgrep-match-face ((nil :foreground "black" :background "#e3f4ff"))))
 ;;(set-face-attribute 'deadgrep-match-face nil :foreground "black" :background "#e3f4ff") ;; https://www.reddit.com/r/emacs/comments/4v7tcj/comment/d5wyu1r/?utm_source=reddit&utm_medium=web2x&context=3
 
-(use-package nano-theme
-  :ensure t
-  :defer
-  :config
-  (custom-theme-set-faces
-   'nano-light
-   '(mode-line-buffer-id ((nil :foreground "black")))
-   '(deadgrep-match-face ((nil :foreground "black" :background "#b4eeb4")))
-   '(selectrum-current-candidate ((nil foreground "black" :background "#b4eeb4"))))
-  (load-theme 'nano-light t))
-
 (use-package monokai-theme
   :ensure t
   :pin melpa
@@ -46,5 +36,5 @@
    '(mode-line-buffer-id ((nil :foreground "goldenrod1")))
    '(deadgrep-match-face ((nil :foreground "black" :background "goldenrod1")))
    '(selectrum-current-candidate ((nil :foreground "black" :background "goldenrod1"))))
-  (load-theme 'monikai t))
+  (load-theme 'monokai t))
 
