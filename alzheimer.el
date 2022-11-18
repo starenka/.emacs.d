79421288b04682c1a2309fc3f62bf5c7aaee53ad
starenka <starenka0@gmail.com>
2022-11-18 09:30:01 +0100

    alzheimer edit+cleanup

--- alzheimer.el
+++ alzheimer.el
@@ -1,28 +1,31 @@
 (setq alzheimer-mode-detailed nil)
+(defconst alzheimer-pills-file (expand-file-name "alzheimer-pills"
+                                                 (cond ((boundp 'user-emacs-directory)
+                                                        user-emacs-directory)
+                                                       ((boundp 'user-init-directory)
+                                                        user-init-directory)
+                                                       (t "~/.emacs.d/"))))
 
-(defface alzheimer-kb-face
-  '((t :inherit 'font-lock-constant-face))
-  "Item kb face")
+(defface alzheimer-kb-face '((t :inherit 'font-lock-constant-face)) "Item kb face")
+(defface alzheimer-item-face '((t :inherit 'font-lock-function-name-face)) "Item commandp face")
+(defface alzheimer-doc-face '((t :inherit 'font-lock-doc-face)) "Item doc face")
+(defface alzheimer-heading-face '((t :inherit 'info-title-2)) "Mode heading face")
+(defface alzheimer-help-face '((t :inherit 'Info-quoted)) "Mode help face")
 
-(defface alzheimer-item-face
-  '((t :inherit 'font-lock-function-name-face))
-  "Item font face")
-
-(defface alzheimer-doc-face
-  '((t :inherit 'font-lock-doc-face))
-  "Item doc face")
-
-(defface alzheimer-heading-face
-  '((t :inherit 'info-title-1))
-  "Mode heading face")
-
-(defun alzheimer--get-pills (path)
+(defun alzheimer--get-pills (&optional path)
   "Reads file line by line or creates empty one"
-  (when (not (file-exists-p path))
-    (with-temp-buffer (write-file path)))
-  (with-temp-buffer
-    (insert-file-contents path)
-    (mapcar 'string-trim (split-string (buffer-string) "\n" t))))
+  (let ((path (or path alzheimer-pills-file)))
+    (when (not (file-exists-p path))
+      (with-temp-buffer (write-file path)))
+    (with-temp-buffer
+      (insert-file-contents path)
+      (mapcar 'string-trim (split-string (buffer-string) "\n" t)))))
+
+(defun alzheimer--edit-pills (&optional path)
+  "Opens pills file in other window"
+  (interactive)
+  (let ((path (or path alzheimer-pills-file)))
+    (find-file-other-window path)))
 
 (defun alzheimer--get-binding (callable)
   "Annotate command with keybinding"
@@ -55,12 +58,17 @@
   (kill-buffer-and-window)
   (alzheimer-show detailed-doc))
 
+(defun alzheimer--toggle-details ()
+  "toggle docstring details"
+  (interactive)
+  (setq alzheimer-mode-detailed (not alzheimer-mode-detailed))
+  (alzheimer--reload alzheimer-mode-detailed))
+
 (defun alzheimer-show (&optional detailed-doc)
   "Creates buffer and shows alzheimer"
   (interactive)
-  (let* ((detailed-doc (or detailed-doc nil))
-        (pills-file (expand-file-name "alzheimer-pills" user-init-dir))
-        (pills (alzheimer--get-pills pills-file)))
+  (let ((detailed-doc (or detailed-doc nil))
+        (pills (alzheimer--get-pills)))
 
     (switch-to-buffer-other-window "*alzheimer*")
     (alzheimer-mode)
@@ -69,15 +77,19 @@
     (if pills (progn
                 (insert (propertize "Take your pill" 'face 'alzheimer-heading-face))
                 (insert "\n\n")
-                (dolist (item (alzheimer--get-pills (expand-file-name "alzheimer-pills" user-init-dir)))
+                (dolist (item (alzheimer--get-pills alzheimer-pills-file))
                   (insert (alzheimer--mk-item item detailed-doc))))
       (progn (insert (propertize "No pills yet :/" 'face 'alzheimer-heading-face))
              (insert "\n\n")
              (insert (format (concat "Add your precious commandps (one line each) to '%s'."
                                      "\nWe will fetch the keybindings and docstring for you. "
-                                     "Just hit `g` to relaod when ready.") pills-file))))
-
-    ;; (insert (format "\n\n%s" (format-time-string "%a %b %d %H:%M:%S %Z %Y" (current-time))))
+                                     "Just hit `g` to relaod when ready.") alzheimer-pills-file))))
+    
+    ;; (insert (propertize
+    ;;          (format "\n\n\ngenerated: %s"
+    ;;                  (format-time-string "%a %b %d %H:%M:%S %Z %Y" (current-time)))
+    ;;          'face
+    ;;          'alzheimer-help-face))
     (setq buffer-read-only t)))
 
 (define-derived-mode alzheimer-mode fundamental-mode "Alzheimer"
@@ -85,9 +97,7 @@
 
 (define-key alzheimer-mode-map (kbd "q") 'kill-buffer-and-window)
 (define-key alzheimer-mode-map (kbd "g") 'alzheimer--reload)
-(define-key alzheimer-mode-map (kbd "d") (lambda ()
-                                           (interactive)
-                                           (setq alzheimer-mode-detailed (not alzheimer-mode-detailed))
-                                           (alzheimer--reload alzheimer-mode-detailed)))
+(define-key alzheimer-mode-map (kbd "d") 'alzheimer--toggle-details)
+(define-key alzheimer-mode-map (kbd "e") 'alzheimer--edit-pills)
 
 (provide 'alzheimer)
