5187df13eb73934a6ddfb459d63a0068928465a6
starenka <starenka0@gmail.com>
2022-11-21 18:58:59 +0100

    my zones

--- global.el
+++ global.el
@@ -375,20 +375,21 @@
 (use-package zone
   :ensure t
   :config
-  (setq zone-programs [zone-pgm-jitter
-                       zone-pgm-putz-with-case
-                       zone-pgm-whack-chars
-                       zone-pgm-rotate
-                       zone-pgm-rotate-LR-lockstep
-                       zone-pgm-rotate-RL-lockstep
-                       zone-pgm-rotate-LR-variable
-                       zone-pgm-rotate-RL-variable
-                       zone-pgm-drip
+  (setq zone-programs [zone-pgm-putz-with-case
                        zone-pgm-drip-fretfully
-                       zone-pgm-five-oclock-swan-dive
-                       zone-pgm-rat-race
-                       zone-pgm-paragraph-spaz
-                       zone-pgm-random-life])
+  ;;                      zone-pgm-jitter
+  ;;                      zone-pgm-whack-chars
+  ;;                      zone-pgm-rotate
+  ;;                      zone-pgm-rotate-LR-lockstep
+  ;;                      zone-pgm-rotate-RL-lockstep
+  ;;                      zone-pgm-rotate-LR-variable
+  ;;                      zone-pgm-rotate-RL-variable
+  ;;                      zone-pgm-drip
+  ;;                      zone-pgm-five-oclock-swan-dive
+  ;;                      zone-pgm-rat-race
+  ;;                      zone-pgm-paragraph-spaz
+  ;;                      zone-pgm-random-life
+                       ])
   (zone-when-idle 300))
 
 ;; startup time profiler
