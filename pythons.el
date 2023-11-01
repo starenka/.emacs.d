f2d06ac47db1b93996a3a51da0ff171e0ec395c8
starenka <starenka0@gmail.com>
2023-11-01 13:30:50 +0100

    doh

--- pythons.el
+++ pythons.el
@@ -1,29 +1,15 @@
-(use-package py-autopep8
-  :ensure t
-  :pin melpa
-  :hook ((python-mode) . py-autopep8-mode))
-
-;;(use-package reformatter
-;;  :hook
-;;  (python-mode . ruff-format-on-save-mode)
-;;  (python-ts-mode . ruff-format-on-save-mode)
-;;  :config
-;;  (reformatter-define ruff-format
-;;    :program "ruff"
-;;    :args `("format" "--stdin-filename" ,buffer-file-name "-")))
-
 (use-package reformatter
   :hook
-  (python-mode . autopep8-on-save-mode)
-  (python-ts-mode . autopep8-on-save-mode)
+  (python-mode . autopep8-format-on-save-mode)
+  (python-ts-mode . autopep8-format-on-save-mode)
   :config
   (reformatter-define autopep8-format
     :program "autopep8"
-    :args '("--in-place" "--hang-closing" "--aggresive 2" "--jobs 0" "--pep8-passes 10000" "-v" buffer-file-name)))
+    :args `("-")))
 
 (add-hook 'python-mode-hook (lambda () ;; M-x devdocs-install
                               (setq-local devdocs-current-docs '(
-                                                                 "django~3.2"
+                                                                 "django~4.2"
                                                                  ;;"elisp"
                                                                  "html"
                                                                  "http"
