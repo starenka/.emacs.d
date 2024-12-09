d55f60c08ec5fd20e3583bf2e181998b312063e0
starenka <starenka0@gmail.com>
2024-12-09 16:55:02 +0100

    cleanup

--- alzheimer.el
+++ alzheimer.el
@@ -1,105 +1,107 @@
-(setq alzheimer-mode-detailed nil)
-(defconst alzheimer-pills-file (expand-file-name "alzheimer-pills"
-                                                 (cond ((boundp 'user-emacs-directory)
-                                                        user-emacs-directory)
-                                                       ((boundp 'user-init-directory)
-                                                        user-init-directory)
-                                                       (t "~/.emacs.d/"))))
+```emacs-lisp
+(defvar alzheimer-mode-detailed nil
+  "Toggle detailed documentation display in Alzheimer mode.")
 
-(defface alzheimer-kb-face '((t :inherit 'font-lock-constant-face)) "Item kb face")
-(defface alzheimer-item-face '((t :inherit 'font-lock-function-name-face)) "Item commandp face")
-(defface alzheimer-doc-face '((t :inherit 'font-lock-doc-face)) "Item doc face")
-(defface alzheimer-heading-face '((t :inherit 'info-title-2)) "Mode heading face")
-(defface alzheimer-help-face '((t :inherit 'Info-quoted)) "Mode help face")
+(defcustom alzheimer-pills-file
+  (expand-file-name "alzheimer-pills"
+                    (or (bound-and-true-p user-emacs-directory)
+                        (bound-and-true-p user-init-directory)
+                        "~/.emacs.d/"))
+  "File to store Alzheimer pills."
+  :type 'file
+  :group 'alzheimer)
+
+(defface alzheimer-kb-face '((t :inherit font-lock-constant-face)) "Face for keybindings in Alzheimer mode.")
+(defface alzheimer-item-face '((t :inherit font-lock-function-name-face)) "Face for items in Alzheimer mode.")
+(defface alzheimer-doc-face '((t :inherit font-lock-doc-face)) "Face for documentation in Alzheimer mode.")
+(defface alzheimer-heading-face '((t :inherit info-title-2)) "Face for headings in Alzheimer mode.")
+(defface alzheimer-help-face '((t :inherit Info-quoted)) "Face for help messages in Alzheimer mode.")
 
 (defun alzheimer--get-pills (&optional path)
-  "Reads file line by line or creates empty one"
+  "Read pills from PATH or `alzheimer-pills-file`. Create file if it does not exist."
   (let ((path (or path alzheimer-pills-file)))
-    (when (not (file-exists-p path))
+    (unless (file-exists-p path)
       (with-temp-buffer (write-file path)))
     (with-temp-buffer
       (insert-file-contents path)
-      (mapcar 'string-trim (split-string (buffer-string) "\n" t)))))
+      (split-string (string-trim (buffer-string)) "\n" t))))
 
 (defun alzheimer--edit-pills (&optional path)
-  "Opens pills file in other window"
+  "Open the pills file located at PATH or `alzheimer-pills-file` in another window."
   (interactive)
-  (let ((path (or path alzheimer-pills-file)))
-    (find-file-other-window path)))
+  (find-file-other-window (or path alzheimer-pills-file)))
 
-(defun alzheimer--get-binding (callable)
-  "Annotate command with keybinding"
-  (when-let* ((sym (intern-soft callable))
+(defun alzheimer--get-binding (command)
+  "Retrieve the keybinding for COMMAND."
+  (when-let* ((sym (intern-soft command))
               (key (and (commandp sym) (where-is-internal sym nil 'first-only))))
     (key-description key)))
 
-(defun alzheimer--get-doc (callable)
-  "Gets callable doc"
-  (when-let ((sym (intern-soft callable)))
-    (documentation sym 'variable-documentation)))
+(defun alzheimer--get-doc (command)
+  "Get the full documentation string for COMMAND."
+  (when-let ((sym (intern-soft command)))
+    (documentation sym)))
 
-(defun alzheimer--get-short-doc (callable)
-  "Gets callable short doc"
-  (when-let ((doc (alzheimer--get-doc callable)))
-    (nth 0 (split-string doc "\n" t))))
+(defun alzheimer--get-short-doc (command)
+  "Get the first line of the documentation string for COMMAND."
+  (when-let ((doc (alzheimer--get-doc command)))
+    (car (split-string doc "\n" t))))
 
 (defun alzheimer--mk-item (item &optional detailed-doc)
-  "Formats item"
-  (let* ((format-string (if detailed-doc (format "%%%ds  %%s %%s\n\n" 10) (format "%%%ds  %%s %%s\n" 10)))
-         (kb (alzheimer--get-binding item))
-         (name item)
-         (doc (if detailed-doc (alzheimer--get-doc item) (alzheimer--get-short-doc item)))
-         (faced-key (propertize (or kb "n/a") 'face 'alzheimer-kb-face))
-         (faced-name (propertize name 'face 'alzheimer-item-face))
+  "Format ITEM with optional DETAILED-DOC."
+  (let* ((format-string (if detailed-doc
+                            " %-10s  %s %s\n\n"
+                          " %-10s  %s %s\n"))
+         (kb (or (alzheimer--get-binding item) "n/a"))
+         (doc (if detailed-doc
+                  (alzheimer--get-doc item)
+                (alzheimer--get-short-doc item)))
+         (faced-key (propertize kb 'face 'alzheimer-kb-face))
+         (faced-name (propertize item 'face 'alzheimer-item-face))
          (faced-doc (propertize (or doc "") 'face 'alzheimer-doc-face)))
     (format format-string faced-key faced-name faced-doc)))
 
 (defun alzheimer--reload (&optional detailed-doc)
-  "Reloads items"
+  "Reload the Alzheimer buffer with optional DETAILED-DOC."
   (interactive)
-  (kill-buffer-and-window)
+  (when (get-buffer "*alzheimer*")
+    (kill-buffer "*alzheimer*"))
   (alzheimer-show detailed-doc))
 
 (defun alzheimer--toggle-details ()
-  "toggle docstring details"
+  "Toggle display of detailed documentation."
   (interactive)
   (setq alzheimer-mode-detailed (not alzheimer-mode-detailed))
   (alzheimer--reload alzheimer-mode-detailed))
 
 (defun alzheimer-show (&optional detailed-doc)
-  "Creates buffer and shows alzheimer"
+  "Display the Alzheimer buffer with optional DETAILED-DOC."
   (interactive)
-  (let ((detailed-doc (or detailed-doc nil))
+  (let ((detailed-doc (or detailed-doc alzheimer-mode-detailed))
         (pills (alzheimer--get-pills)))
-
     (switch-to-buffer-other-window "*alzheimer*")
     (alzheimer-mode)
     (erase-buffer)
-
-    (if pills (progn
-                (insert (propertize "Take your pill" 'face 'alzheimer-heading-face))
-                (insert "\n\n")
-                (dolist (item (alzheimer--get-pills alzheimer-pills-file))
-                  (insert (alzheimer--mk-item item detailed-doc))))
-      (progn (insert (propertize "No pills yet :/" 'face 'alzheimer-heading-face))
-             (insert "\n\n")
-             (insert (format (concat "Add your precious commandps (one line each) to '%s'."
-                                     "\nWe will fetch the keybindings and docstring for you. "
-                                     "Just hit `g` to relaod when ready.") alzheimer-pills-file))))
-    
-    ;; (insert (propertize
-    ;;          (format "\n\n\ngenerated: %s"
-    ;;                  (format-time-string "%a %b %d %H:%M:%S %Z %Y" (current-time)))
-    ;;          'face
-    ;;          'alzheimer-help-face))
+    (if pills
+        (progn
+          (insert (propertize "Take your pill" 'face 'alzheimer-heading-face))
+          (insert "\n\n")
+          (dolist (item pills)
+            (insert (alzheimer--mk-item item detailed-doc))))
+      (progn
+        (insert (propertize "No pills yet :/" 'face 'alzheimer-heading-face))
+        (insert "\n\n")
+        (insert (format "Add your commands (one per line) to '%s'.\nWe will fetch keybindings and documentation for you. Press `g` to reload when ready."
+                        alzheimer-pills-file))))
     (setq buffer-read-only t)))
 
 (define-derived-mode alzheimer-mode fundamental-mode "Alzheimer"
-  "Set major mode for viewing alzheimer's notes.")
+  "Major mode for viewing Alzheimer pills."
+  (use-local-map alzheimer-mode-map))
 
-(define-key alzheimer-mode-map (kbd "q") 'kill-buffer-and-window)
-(define-key alzheimer-mode-map (kbd "g") 'alzheimer--reload)
-(define-key alzheimer-mode-map (kbd "d") 'alzheimer--toggle-details)
-(define-key alzheimer-mode-map (kbd "e") 'alzheimer--edit-pills)
+(define-key alzheimer-mode-map (kbd "q") #'kill-buffer-and-window)
+(define-key alzheimer-mode-map (kbd "g") #'alzheimer--reload)
+(define-key alzheimer-mode-map (kbd "d") #'alzheimer--toggle-details)
+(define-key alzheimer-mode-map (kbd "e") #'alzheimer--edit-pills)
 
 (provide 'alzheimer)
