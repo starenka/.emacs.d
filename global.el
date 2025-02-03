5a586844fc93026ed89ba43bc6454222fe6c84be
starenka <starenka0@gmail.com>
2025-02-03 22:40:16 +0100

    +ultrascroll

--- global.el
+++ global.el
@@ -522,3 +522,12 @@
 ;;(straight-use-package
 ;; '(gptel-quick :type git :host github :repo "karthink/gptel-quick"))
 
+;; (package-vc-install '(ultra-scroll :vc-backend Git :url  "https://github.com/jdtsmith/ultra-scroll"))
+(use-package ultra-scroll
+  :ensure t
+  ;:load-path "~/code/emacs/ultra-scroll" ; if you git clone'd instead of package-vc-install
+  :init
+  (setq scroll-conservatively 101 ; important!
+        scroll-margin 0)
+  :config
+  (ultra-scroll-mode 1))
