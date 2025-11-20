4e1349dfb85f04ab80e51caf75bf7de739241f6a
starenka <starenka0@gmail.com>
2025-11-20 20:44:27 +0100

    cleanup

--- term.el
+++ term.el
@@ -10,22 +10,17 @@
   :config
   (setq multi-vterm-dedicated-window-height-percent 32))
 
-(add-hook 'term-mode-hook (lambda ()
-                            (setq term-buffer-maximum-size 50000)
-                            (define-key term-raw-map (kbd "C-y") 'term-paste)
-                            (yas-minor-mode -1) ;; workaround a bug in yas
-                            (abbrev-mode -1)
-                            ;;(show-paren-mode -1)
-                            (electric-indent-local-mode -1)
-                            ))
+(defun my-term-shell-setup ()
+  (when (derived-mode-p 'term-mode)
+    (setq term-buffer-maximum-size 50000)
+    (define-key term-raw-map (kbd "C-y") 'term-paste)
+    (yas-minor-mode -1) ;; workaround a bug in yas
+    (abbrev-mode -1)
+    (electric-indent-local-mode -1))
+  (ansi-color-for-comint-mode-on)
+  (display-line-numbers-mode -1)
+  (nlinum-mode -1)
+  (setq-local global-hl-line-mode nil))
 
-(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
-
-(dolist (mode '(term-mode-hook
-                vterm-mode-hook
-                shell-mode-hook
-                eshell-mode-hook))
-  (add-hook mode (lambda()
-                   (display-line-numbers-mode 0)
-                   (nlinum-mode -1)
-                   (setq-local global-hl-line-mode nil))))
+(dolist (hook '(term-mode-hook vterm-mode-hook shell-mode-hook eshell-mode-hook))
+  (add-hook hook #'my-term-shell-setup))
