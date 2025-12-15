e97c0505ade540fa8673103f02aafaa97f9a2313
starenka <starenka0@gmail.com>
2025-12-15 16:59:05 +0100

    disable ruff

--- lsp.el
+++ lsp.el
@@ -21,8 +21,8 @@
    lsp-file-watch-threshold 2000
    ;; uv pip install -U 'python-lsp-server[all,websockets]' python-lsp-isort pylsp-rope
    lsp-pylsp-server-command "/data/.envs/emacs-pylsp/bin/pylsp"
-   lsp-pylsp-plugins-ruff.enabled t
    lsp-pylsp-configuration-sources ["flake8"]
+   lsp-pylsp-plugins-ruff.enabled nil
    lsp-pylsp-plugins-pycodestyle-enabled nil
    lsp-pylsp-plugins-pydocstyle-enabled nil
    lsp-pylsp-plugins-mccabe-enabled nil
